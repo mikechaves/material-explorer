f7c0aa21e92e13f89772ad49c8cbf6947fd7ae17
mikechaves <163567591+mikechaves@users.noreply.github.com>
2024-03-15 12:25:23 -0700

    Wrapped App with MaterialProvider

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// src/App.tsx
+import React from 'react';
+import './App.css'; // Assuming you're using CSS for styles
+import { MaterialProvider } from './contexts/MaterialContext';
+import MaterialEditor from './components/MaterialEditor';
+// Import other components as needed
+
+function App() {
+  return (
+    <MaterialProvider>
+      <div className="App">
+        <header className="App-header">
+          {/* Your app header here */}
+        </header>
+        <main>
+          {/* Wrap the entire component tree that needs access to the MaterialContext */}
+          <MaterialEditor />
+          {/* You can add other components here that should have access to the MaterialContext */}
+        </main>
+      </div>
+    </MaterialProvider>
+  );
+}
+
+export default App;
