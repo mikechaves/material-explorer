55a95e4f9e009226ab0f03e4eecdc55542cf2ddc
mikechaves <163567591+mikechaves@users.noreply.github.com>
2024-03-15 12:06:14 -0700

    Updated MaterialContext to setup Sidebar component

---
+++ src/contexts/MaterialContext.tsx
@@ -0,0 +1,44 @@
+// src/contexts/MaterialContext.tsx
+
+import React, { createContext, useState, useContext } from 'react';
+
+interface Material {
+  id: string;
+  color: string;
+  metalness: number;
+  roughness: number;
+}
+
+interface MaterialContextType {
+  materials: Material[];
+  addMaterial: (material: Material) => void;
+  removeMaterial: (id: string) => void;
+}
+
+const MaterialContext = createContext<MaterialContextType | undefined>(undefined);
+
+export const MaterialProvider: React.FC = ({ children }) => {
+  const [materials, setMaterials] = useState<Material[]>([]);
+
+  const addMaterial = (material: Material) => {
+    setMaterials((prevMaterials) => [...prevMaterials, material]);
+  };
+
+  const removeMaterial = (id: string) => {
+    setMaterials((prevMaterials) => prevMaterials.filter((material) => material.id !== id));
+  };
+
+  return (
+    <MaterialContext.Provider value={{ materials, addMaterial, removeMaterial }}>
+      {children}
+    </MaterialContext.Provider>
+  );
+};
+
+export const useMaterials = () => {
+  const context = useContext(MaterialContext);
+  if (context === undefined) {
+    throw new Error('useMaterials must be used within a MaterialProvider');
+  }
+  return context;
+};
